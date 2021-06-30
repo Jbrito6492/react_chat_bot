def55270dacdeab0851012eb4afa61147021175b
Josh Brito <jbrito6492@gmail.com>
2021-06-29 22:58:33 -0700

    dev edits

--- src/components/ChatContent.jsx
+++ src/components/ChatContent.jsx
@@ -53,7 +53,7 @@ export default function ChatContent(props) {
         <div className={styles.blocks}>
           <div className={styles["current-chatting-user"]}>
             <Avatar styles={styles} isOnline="active" image={img} />
-            <p>{currentBot}</p>
+            <p>{currentBot} (se habla espanol)</p>
           </div>
         </div>
 
@@ -83,24 +83,31 @@ export default function ChatContent(props) {
         </div>
       </div>
       <div className={styles.content__footer}>
-        <div className={styles.sendNewMessage}>
-          {/* <button className={styles.addFiles}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.sendNewMessage}>
+            {/* <button className={styles.addFiles}>
             <BiPlus size={20} disabled={true} />
           </button> */}
-          <input
-            type="text"
-            placeholder={currentBot === "Raul Inglesias" ? "Type a message here" : "Preguntale a tu papa"}
-            onChange={onStateChange}
-            value={state.userMessage.msg}
-          />
-          <button
-            className={styles.btnSendMsg}
-            id="sendMsgBtn"
-            onClick={handleSubmit}
-          >
-            <BiPaperPlane size={20} />
-          </button>
-        </div>
+            <input
+              type="text"
+              placeholder={
+                currentBot === "Raul Inglesias"
+                  ? "Type a message here"
+                  : "Preguntale a tu papa"
+              }
+              onChange={onStateChange}
+              value={state.userMessage.msg}
+              onSubmit={handleSubmit}
+            />
+            <button
+              className={styles.btnSendMsg}
+              id="sendMsgBtn"
+              onClick={handleSubmit}
+            >
+              <BiPaperPlane size={20} />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
